ab59b6a7dc2bc1f27754625926122c0b0411137a
Eric <ela57@sfu.ca>
2019-11-28 17:33:48 -0800

    date column parsed

--- program.py
+++ program.py
@@ -5,9 +5,12 @@ from datetime import date
 
 in_directory = 'googleplaystore.csv'
 
-data = pd.read_csv(in_directory,parse_dates=['Last Updated'])
+data = pd.read_csv(in_directory)
+data = data.dropna()
 
 data['Price'] = data['Price'].str.extract(r'(\d+.\d+)')[0].astype('float32')
 data['Price'] = data['Price'].fillna(0)
+
+data['Last Updated'] = pd.to_datetime(data['Last Updated'], format='%B %d, %Y')
 
 paid = data[data['Type'] == "Paid"]
