5dd7eb503d8e1d37e8c70a9d7faebfe3c6f281c8
jtan99 <jtantan99@gmail.com>
2019-11-29 23:56:27 -0800

    changed test to use normalized score

--- README.md
+++ README.md
@@ -1,16 +1,26 @@
 # CMPT-353-Project
 Program requirements:
-python 3.x
+	python 3.x
+
+If you do not have the following libraries installed, Please enter the corresponding commands
+
 scikit-posthocs 0.6.1
+
+	pip install scikit-posthocs
+
 scipy
+
+	pip install scipy
 
 Start up:
 create a local directory by entering the following command in a terminal
 
-git clone https://github.com/eriekl6/CMPT-353-Project.git
+	git clone https://github.com/eriekl6/CMPT-353-Project.git
 
 change into the folder containing the project
-cd CMPT-353-project
+
+	cd CMPT-353-project
 
 To run the program enter the following command
-python program.py
+
+	python program.py
--- program.py
+++ program.py
@@ -3,6 +3,7 @@ import numpy as np
 import datetime
 import scipy
 import scikit_posthocs as sp
+import matplotlib.pyplot as plt
 from datetime import date
 from scipy import stats
 
@@ -20,7 +21,27 @@ data['Installs'] = data['Installs'].str.replace(r'[\+,]','').astype('int64')
 data = data[data['Installs']>1000]
 
 #parse the 'Last Updated' column to datetime type
-data['Last Updated'] = pd.to_datetime(data['Last Updated'], format='%B %d, %Y')	
+data['Last Updated'] = pd.to_datetime(data['Last Updated'], format='%B %d, %Y')
+data['Reviews'] = data['Reviews'].astype(int)
+
+# --calculating a rank score --
+# -- d is number of reviews for the app
+d = data['Reviews']
+
+# -- R is average rating for the APP
+R = data['Rating']
+
+# -- m is min number of reviews to be listed in top 250
+top250 = data.sort_values('Reviews',ascending=False)
+top250 = top250[:250]
+m = top250['Reviews'].iloc[249]
+
+# -- C is the mean amount of reviews across whole
+C = data['Reviews'].mean()
+
+score = (d/(d+m))*R+(m/(d+m))
+data['score'] = score
+# --rank score end --
 
 paid = data[data['Type'] == "Paid"]		# table of paid apps only
 
@@ -32,6 +53,7 @@ paid_timestamp = paid['Last Updated'].apply(to_timestamp)
 #Do more recent/ recently updated apps cost more?
 time_price_fit = stats.linregress(paid_timestamp,paid['Price'])
 # time_rating_fit = stats.linregress(paid_timestamp,paid['Rating'])
+
 
 price_predict = paid_timestamp*time_price_fit.slope + time_price_fit.intercept
 
@@ -51,47 +73,46 @@ plt.savefig('Time vs Price')
 	# plt.ylabel('Price')
 	# plt.title('Change in Rating over time')
 
-category = data[['Category','App','Installs']].copy()
+
+category = data[['Category','App','score']].copy()
 rank = category.groupby(['Category']).agg('count').sort_values('App', ascending = False)
 # group = data.groupby(['Category','Installs']).agg('count')
 
+# -- creating a bar graph for the market share --
+bargraph = pd.DataFrame()
+bargraph['Count'] = rank['App']
+print(bargraph)
+
+plt.figure()
+bargraph.plot.bar()
+sizes = np.array(rank['App'])
+plt.savefig('marketShare')
+
 # --getting the top 5 --
 rankLst = rank.index.values.tolist()
-
 rank1 = category[category['Category'] == rankLst[0]].reset_index()
 rank2 = category[category['Category'] == rankLst[1]].reset_index()
 rank3 = category[category['Category'] == rankLst[2]].reset_index()
 rank4 = category[category['Category'] == rankLst[3]].reset_index()
 rank5 = category[category['Category'] == rankLst[4]].reset_index()
 
-var = stats.levene(rank1['Installs'],rank2['Installs']).pvalue
+# -- checking is the variance equal for the groups --
+var = stats.levene(rank1['score'],rank2['score']).pvalue
 print('Levene test for equal variance p-value:', var)
 
 # -- doing an kruskal test, similar to anova but for unequal var and sample size
-Kruskal = stats.kruskal(rank1['Installs'], rank2['Installs'], rank3['Installs'], rank4['Installs'], rank5['Installs'])
+Kruskal = stats.kruskal(rank1['score'], rank2['score'], rank3['score'], rank4['score'], rank5['score'])
 print(Kruskal)
 # print(Kruskal.pvalue)
 
 # -- Doing a posthoc tamhane test, similar to tukey but for unequal var and sample size --
-x = pd.concat([rank1['Installs'],rank2['Installs'],rank3['Installs'],rank4['Installs'],rank5['Installs']], ignore_index=True, axis=1)
+x = pd.concat([rank1['score'],rank2['score'],rank3['score'],rank4['score'],rank5['score']], ignore_index=True, axis=1)
 x = x.melt(var_name='groups', value_name='values')
 posthoc = sp.posthoc_tamhane(x, val_col='values', group_col='groups')
 
 print(posthoc)
-
-# -- d is number of reviews for the app
-d = data['Reviews']
-
-# -- R is average rating for the APP
-R = data['Rating']
-
-# -- m is min number of reviews to be listed in top 250
-top250 = data.sort_values('Reviews',ascending=False)
-top250 = top250[:250]
-m = top250['Reviews'].iloc[249]
-
-# -- C is the mean amount of reviews across whole
-C = data['Reviews'].mean()
-
-score = (d/(d+m))*R+(m/(d+m))
-data['score'] = score
+X = sp.sign_array(posthoc).astype('int32')
+print(X)	# Numpy array where 0 is False (not significant), 1 is True (significant)
+plt.figure()
+sp.sign_plot(X, flat = True)
+plt.savefig('posthoc')
